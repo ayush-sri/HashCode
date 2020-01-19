39d900d03ab8af9f0552e40bc875bfb622aaca23
Tushar Tripathi <53949909+coding-viper@users.noreply.github.com>
2020-01-19 13:37:29 +0530

    Update PizzaProb.py

--- PizzaProb.py
+++ PizzaProb.py
@@ -1,1 +1,17 @@
 #implement the function for pizza 
+def fun(inp):
+	limit,variety=map(int,inp[0].split())
+	inp_pizza=list(map(int,inp[1].split()))
+	assert inp_pizza.__len__()==variety,'The variety of pizza cannot exceed the input variety'
+	dic=dict()
+	j=inp_pizza.__len__()-1
+	while(j>=0):
+		lis=[]
+		i=j
+		while(i>=0):
+			if (inp_pizza[i]+sum(lis))<=limit:
+				lis.append(inp_pizza[i])
+			i-=1
+		dic[sum(lis)]=lis[::-1]
+		j-=1
+	return dic,[inp_pizza.index(i) for i in dic[max(dic.keys())]].__len__(),[inp_pizza.index(i) for i in dic[max(dic.keys())]]
